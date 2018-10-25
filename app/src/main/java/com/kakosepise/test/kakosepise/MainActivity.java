db3854b3a24896587d4118429d3c71fcd2f7c0b0
skilful-sovereign <xlr80000@gmail.com>
2018-10-25 22:48:35 +0200

    Initial commit

---
+++ app/src/main/java/com/kakosepise/test/kakosepise/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kakosepise.test.kakosepise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
