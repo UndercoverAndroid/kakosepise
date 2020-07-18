f8e87aa271f6c15e49f8d2e0c0159dafec61eed2
skilful-sovereign <xlr80000@gmail.com>
2020-07-18 18:50:52 +0200

    Added REST functionality

--- app/src/main/java/com/kakosepise/test/kakosepise/MainActivity.java
+++ app/src/main/java/com/kakosepise/test/kakosepise/MainActivity.java
@@ -9,6 +9,18 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,7 +29,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button m_toastButton, m_viewAllButton;
+    Button m_toastButton, m_viewAllButton, m_restButton;
     ListView m_list;
     ArrayAdapter m_customerArrayAdapter;
     DatabaseController m_db;
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         m_list = findViewById(R.id.list_view);
         m_toastButton = findViewById(R.id.button_init);
         m_viewAllButton = findViewById(R.id.button2);
+        m_restButton = findViewById(R.id.rest_btn);
 
         // Database initialization
         // Step 1 - We create an empty database
@@ -56,6 +69,52 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 showCustomersInListView();
+
+            }
+        });
+
+        m_restButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://kakosepise.com/wp-json/wp/v2/ksp_rec/?page=1";
+
+                JsonArrayRequest m_jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray m_response) {
+                        String m_searchTerm = "";
+                        String m_title = "";
+                        String m_content = "";
+                        int m_ID = 0;
+                        int m_i =0;
+                        int m_done = 0;
+                        String m_cmpDate = "2019-05-17T10:09:28";
+                        String m_date = "";
+                        try {
+                            JSONObject m_term = m_response.getJSONObject(0);
+                            m_searchTerm = m_term.getString("slug");
+                            JSONObject m_tmpTitle = m_term.getJSONObject("title");
+                            m_title = m_tmpTitle.getString("rendered");
+                            m_ID = m_term.getInt("id");
+                            JSONObject m_tmpContent = m_term.getJSONObject("content");
+                            m_content = m_tmpContent.getString("rendered");
+                            m_date = m_term.getString("date");
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MainActivity.this, m_title + m_date, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                RESTSingleton.getInstance(MainActivity.this).addToRequestQueue(m_jsonArrayRequest);
 
             }
         });
---
+++ app/src/main/java/com/kakosepise/test/kakosepise/RESTService.java
@@ -0,0 +1,19 @@
+package com.kakosepise.test.kakosepise;
+
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RESTService {
+
+
+
+
+}
